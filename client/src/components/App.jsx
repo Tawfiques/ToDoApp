621bcc7b0472ff59ba9bbe84edb4cdee5236e889
Tawfiques <tawfiquehassan@gmail.com>
2024-05-30 00:09:02 +0600

    ToDoApp Body Added

---
+++ client/src/components/App.jsx
@@ -0,0 +1,12 @@
+import { Grid } from "@mui/material";
+import Body from "./Body";
+export default function App() {
+  return (
+    <div>
+      <Grid container>
+        <Grid item xs={12}></Grid>
+      </Grid>
+      <Body />
+    </div>
+  );
+}
