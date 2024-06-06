f6d234405227a75bd4443fbfd65f578b104c12a5
Tawfiques <tawfiquehassan@gmail.com>
2024-06-06 19:10:12 +0600

    app proxy set to 1

--- server/api/index.js
+++ server/api/index.js
@@ -24,6 +24,7 @@ app.use(cors({
   credentials: true, // Allow credentials
   optionsSuccessStatus: 200
 }));
+app.set("trust proxy", 1);
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(morgan('dev'))
 function authMiddleware(req, res, next) {
