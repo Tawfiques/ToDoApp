df9c37b39509d585c9624383ed38965a20245bb2
Tawfiques <tawfiquehassan@gmail.com>
2024-06-06 23:48:11 +0600

    Cookies cleared

--- server/api/index.js
+++ server/api/index.js
@@ -46,8 +46,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     store: MBSTORE,
-    cookie: {
-      sameSite: 'none',
+    cookie: { // Secure cookies in production
       maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
     }
   })
